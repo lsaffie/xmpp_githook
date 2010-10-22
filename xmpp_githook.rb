b3ab45f08ea5ea48fb091b0dfc85b555c7df787c
Luis Saffie <luis.saffie@ehealthinnovation.org>
2010-10-22 10:56:02 -0400

    Remove debugger

--- xmpp_githook.rb
+++ xmpp_githook.rb
@@ -13,10 +13,17 @@ class XmppGithook < Sinatra::Base
 
   post '/' do
     post = params[:payload]
-    require 'ruby-debug'
-    debugger
+    message = parse_post(post)
     im = Jabber::Simple.new("user@jabber.telemonitoring.ca", "user")
-    im.deliver("lsaffie@jabber.telemonitoring.ca", post.inspect)
+    im.deliver("lsaffie@jabber.telemonitoring.ca", message)
+  end
+
+  def parse_post(post)
+    json = JSON.parse(post)
+    commit = json["commits"][0]
+    repo = json["repository"]
+    string = "#{commit['author']['name']} commited to #{repo["name"]}"
+    string
   end
 end
 
